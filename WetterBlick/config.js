2afc793b94a18a6cad3a1f2c2f70580ce63a7080
Sam <sameer1321khan@gmail.com>
2024-08-28 02:16:34 +0530

    version 0.3 added daily forecast feature

--- WetterBlick/config.js
+++ WetterBlick/config.js
@@ -1,4 +1,3 @@
 const API_KEY = "8cd395a090e7470c81074157213105";
 const PVT_TOKEN = "pk.e58b3360d566d4270e8a82313253b09f";
-
-export { API_KEY , PVT_TOKEN};
+export { API_KEY , PVT_TOKEN };
--- WetterBlick/script.js
+++ WetterBlick/script.js
@@ -1,5 +1,6 @@
-import { API_KEY ,PVT_TOKEN} from "./config.js"
+import { API_KEY ,PVT_TOKEN } from "./config.js"
 
+//setting and selecting div value for current weather
 let body = document.querySelector("body")
 let temperature = document.querySelector(".temperature");
 let humidity = document.querySelector(".humidity");
@@ -18,6 +19,22 @@ let condition = document.querySelector(".conditionText");
 let heatindex = document.querySelector(".heatindex");
 let searchbutton = document.querySelector(".get-weather")
 let search = document.querySelector("#city");
+let today = document.querySelector(".today")
+let daily = document.querySelector(".daily")
+let todayBtn = document.querySelector("#todayButton")
+let dailyBtn = document.querySelector("#dailyButton")
+
+
+dailyBtn.addEventListener("click",()=>{
+    today.style.display="none"
+    daily.style.display="flex"
+
+})
+todayBtn.addEventListener("click",()=>{
+    daily.style.display="none"
+    today.style.display="flex"
+})
+
 function getCity(coordinates) { 
     return new Promise((resolve, reject) => {
         var xhr = new XMLHttpRequest(); 
@@ -61,15 +78,11 @@ function main() {
 			// console.log(city);
 			
 			setValues(city);
+            set3daysValue(city,3)
 		}
 		)
-		
-		
-		
 		return; 
-
-	} 
-
+	}
 	function error(err) { 
 		console.warn(`ERROR(${err.code}): ${err.message}`); 
 	} 
@@ -80,6 +93,8 @@ function main() {
 
 
 main();
+
+
 search.addEventListener('keydown', function(event) {
     // Check if the Enter key is pressed
     if (event.key === 'Enter') {
@@ -96,7 +111,133 @@ searchbutton.addEventListener('click',async(e)=>{
         return;
     }
     setValues(city);
+    set3daysValue(city,3);
 })
+
+async function set3daysValue(city,days=3) {
+    if (city === "") {
+        alert("Please Enter a city name..");
+        return;
+    }
+    let getWeather = await fetch3DaysWeather(city,days);
+    let data = getWeather.forecast.forecastday
+    let location = getWeather.location;
+    
+    //updating header
+    let time = document.querySelector(".timeDaily")
+    let locationValue = document.querySelector(".locationvalueToday")
+    time.innerHTML = data[0].date
+    locationValue.textContent =" "+ location.name +" "+location.region+" "+ location.country;
+    // updating daily forecast
+    // console.log(data[0].hour[18]);
+    
+    let comp = document.querySelector(".contentOverlay");
+    const children = comp.children;
+    console.log(comp);
+    // Loop through the children and remove all except the first one
+    while (children.length > 1) {
+    comp.removeChild(children[1]); // Always remove the second child
+    }
+    console.log(comp);
+    for (let i = 0; i < getWeather.forecast.forecastday.length; i++) {
+        let date = new Date(data[i].date)
+    let parts= date.toString().split(' ');
+    
+    date = `${parts[0]} ${parts[1]} ${parts[2]}`;
+        comp.innerHTML+=`<div class="line" style="margin-top: 20px;"></div>
+                <div class="component">
+                    <div class="undetailedInfo">
+                        <div class="tabOne">
+                            <span class="dateDaily">${date}</span>
+                            <span class="temptoday"><span class="todayCurrrentTemp highesttemptoday">${data[i].day.maxtemp_c}</span>/<span class="lowesttemptoday">${data[i].day.mintemp_c}</span></span>
+                            <span class="conditiontoday"><img class="weather-img" src="${data[i].day.condition.icon}" alt="Weather"><span>${data[i].day.condition.text}</span></span>
+                        </div>
+                        <div class="tabTwo">
+                            <span><span class="material-symbols-outlined" style="font-size: unset;">
+                                humidity_percentage
+                                </span> <span>${data[i].day.avghumidity}%</span></span>
+                            <span><span class="material-symbols-outlined" style="font-size: unset;">
+                                air
+                                </span> ${data[0].day.maxwind_kph} km/h</span>
+                            <span class="material-symbols-outlined" style="font-size:30px;">
+                                keyboard_arrow_up
+                                </span>
+                        </div>
+                    </div>
+                    <div class="detailedInfo" style="display: none;">
+                        <div class="arrowDown"><span class="material-symbols-outlined" style="font-size:30px;">
+                            keyboard_arrow_up
+                            </span></div>
+                        <div class="cardContainer">
+                            <div class="card">
+                                <div class="dayTime">${parts[0]} ${parts[2]} | Day</div>
+                                <div class="tempdetails">
+                                    <div class="tempdetailed">
+                                    <div class="temperature3rdday">${data[i].day.maxtemp_c} °C</div>
+                                            <img class="weather-img" src="${data[i].day.condition.icon}" alt="Weather">
+                                    </div>
+                                    <div class="setAndRise">
+                                        <div class="sunrise">Sun Rise: ${data[i].astro.sunrise}</div>
+                                        <div class="sunset">Sun Set: ${data[i].astro.sunset}</div>
+                                    </div>
+                                
+                                </div>
+                                <div class="conditiondetailed">
+                                    Mist
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="dayTime">${parts[0]} ${parts[2]} | Night</div>
+                                <div class="tempdetails">
+                                    <div class="tempdetailed">
+                                    <div class="temperature3rdday">${data[i].day.mintemp_c} °C</div>
+                                            <img class="weather-img" src="${data[i].hour[18].condition.icon}" alt="Weather">
+                                    </div>
+                                    <div class="setAndRise">
+                                        <div class="sunrise">Moon Rise: ${data[i].astro.moonrise}</div>
+                                        <div class="sunset">Moon Set: ${data[i].astro.moonset}</div>
+                                    </div>
+                                
+                                </div>
+                                <div class="conditiondetailed">
+                                ${data[i].hour[18].condition.text}
+                                </div>
+                            </div>
+                        
+                        </div>
+                    </div>
+                </div>`
+    }
+    //setting and selecting div value for daily weather
+    let components = document.querySelectorAll(".component")
+    // console.log(components);
+
+    // css 
+    components.forEach((component)=>{
+        let showing = false;
+    component.addEventListener("click",()=>{
+        let undetailedCard = component.querySelector('.undetailedInfo')
+        let detailedCard = component.querySelector('.detailedInfo')
+        
+        if(!showing){
+            undetailedCard.style.display='none';
+            detailedCard.style.display="block"
+            showing = true;
+            // console.log("shoiwng");
+            return;
+        }
+        if(showing){
+            undetailedCard.style.display='flex';
+            detailedCard.style.display="none"
+            showing = false;
+            // console.log("hiding");
+            return;
+        }
+        console.log("clicking", showing);
+        
+    })
+    })
+}
 async function setValues(city) {
 	if (city === "") {
         alert("Please Enter a city name..");
@@ -181,10 +322,11 @@ const fetchCurrentWeather = async (city)=>{
 const fetch3DaysWeather = async (city, days) => {
     try {
         const baseURL = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=${days}&aqi=no&alerts=no`;
+        // const baseURL = `api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&cnt=${days}&appid=${API_KEY_OPEN}`;
         const response = await fetch(baseURL);
         if (!response.ok) throw new Error('City not found');
         const data = await response.json();
-        console.log(data);
+        // console.log(data);
         return data
     } catch (error) {
         alert(error.message);
