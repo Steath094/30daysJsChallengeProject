a890bf7544f7c9d5d97171fb57de1f3320c7b0b7
Sam <armandawoodkhan@gmail.com>
2024-09-26 16:40:16 +0530

    all functionn working

--- GameProject/FlappyBird/script.js
+++ GameProject/FlappyBird/script.js
@@ -3,7 +3,17 @@ let board;
 let boardWidth = 360;
 let boardHeight = 640;
 let context;
-
+function isMobileDevice() {
+    return /Mobi|Android|iPhone/i.test(navigator.userAgent) || window.innerWidth <= 430;
+}
+  
+  if (isMobileDevice()){
+    boardHeight=window.screen.height;
+    boardWidth = window.screen.width;
+  }else{
+    boardHeight=640
+    boardWidth=360;
+  }
 //bird
 let birdWidth = 34;
 let birdHeight = 24;
@@ -25,6 +35,17 @@ let pipeX = boardWidth;
 let pipeY = 0;
 let topPipeImg;
 let bottomPipeImg;
+
+
+//physics
+let velocityX = -2;
+let velocityY = 0; //bird jump speed
+let gravity = 0.4;
+
+
+let gameOver = false;
+let score = 0;
+
 window.onload = function() {
     board = document.getElementById('board');
     board.width = boardWidth;
@@ -51,34 +72,94 @@ window.onload = function() {
 
     requestAnimationFrame(update)
     setInterval(placePipes,1500);
+    document.addEventListener("keydown",moveBird);
+    document.addEventListener("click",moveBird);
 }
 
 function update() {
     requestAnimationFrame(update);
+    if(gameOver) return;
     context.clearRect(0,0,board.width,board.height);
 
     //bird
+    velocityY +=gravity;
+    // bird.y += velocityY;
+    bird.y  = Math.max(bird.y + velocityY,0); //apply gravity to current bird.y, limi the bird.y to top of the canvas
     context.drawImage(birdImg,bird.x,bird.y,bird.width,bird.height)
+
+    if (bird.y>board.height) {
+        gameOver = true;
+    }
     //pipe
     for(let i = 0; i < pipeArray.length; i++){
         let pipe = pipeArray[i];
+        pipe.x+=velocityX;
         context.drawImage(pipe.img,pipe.x,pipe.y,pipe.width,pipe.height);
-        context.drawImage(pipe,pipe.x,pipe.y+pipe.height,pipe.width,pipe.height);
+        // context.drawImage(pipe,pipe.x,pipe.y+pipe.height,pipe.width,pipe.height);
+        if (!pipe.passed && bird.x>pipe.x+pipe.width) {
+            score +=0.5;
+            pipe.passed=true;
+        }
+        if(detectCollison(bird,pipe)){
+            gameOver = true;
+        }
+    }
+    while (pipeArray.length>0 && pipeArray[0].x+pipeArray[0].width<0) {
+        console.log(pipeArray);
+        
+        pipeArray.shift() // removes first element from the array
+    }
+    //score
+    context.fillstyle = "white";
+    context.font = "45px sans-serif";
+    context.fillText(score,5,45);
+
+    if (gameOver) {
+        context.fillText("Game Over",5,90)
     }
 }
 function placePipes(){
+    if (gameOver) {
+        return;
+    }
+    let randomPipeY = pipeY -pipeHeight/4 -Math.random()*pipeHeight/2;
+    let openingSpace = boardHeight/4;
     let topPipe = {
         img: topPipeImg,
         x:pipeX,
-        y:pipeY,
+        y:randomPipeY,
         width:pipeWidth,
         height:pipeHeight,
         passed: false
     }
     pipeArray.push(topPipe);
     let bottomPipe = {
+        img:bottomPipeImg,
         x:pipeX,
-        y:pipeY+pipeHeight+100
+        y:randomPipeY+pipeHeight+openingSpace,
+        width:pipeWidth,
+        height:pipeHeight,
+        passed:false
     }
     pipeArray.push(bottomPipe);
+}
+function moveBird(e){
+    console.log(e.type);
+    
+    if (e.code =='Space' || e.code =='ArrowUp' || e.type =='click') {
+        velocityY = -6;
+        if (gameOver) {
+            bird.y=birdY;
+            pipeArray = [];
+            score = 0;
+            gameOver = false;
+        }
+    }
+}
+
+function detectCollison(a,b){
+    return a.x < b.x + b.width &&
+     a.x+a.width>b.x &&
+     a.y<b.y+b.height &&
+     a.y + a.height>b.y;
 }
